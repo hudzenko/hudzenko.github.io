02297905c889456f0346252d443bacb666bbd9d4
Evhenii Hudzenko <ukraineboyzheka@gmail.com>
2016-02-26 02:57:53 +0200

    commit one

---
+++ js/common.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+	function heightDetect(){
+		$('.main_head').css('height',$(window).height());
+	}
+	heightDetect();
+	$(window).resize(function(){
+		heightDetect();
+	});
+
+	$('.toggle_mnu').click(function() {
+  	$('.sandwich').toggleClass('active');
+	});
+
+	$('.top_mnu ul a').click(function(){
+		$('.top_mnu').fadeOut(600);
+			$('.top_mnu li a').removeClass('fadeInUp animated')
+			$('.sandwich').toggleClass('active');	
+	});
+	$('.toggle_mnu').click(function(){
+		if($('.top_mnu').is(':visible')){
+			$('.top_text').removeClass('h_opacity');
+			$('.top_mnu').fadeOut(600);
+			$('.top_mnu li a').removeClass('fadeInUp animated')	
+		}else{
+			$('.top_text').addClass('h_opacity');
+			$('.top_mnu').fadeIn(600);
+			$('.top_mnu li a').addClass('fadeInUp animated')
+		}	
+	});
+	
+});
+$(window).load(function() { 
+	$('.loader_inner').fadeOut(); 
+	$('.loader').delay(400).fadeOut('slow'); 
+});
