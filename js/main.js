d7fb941e9312dc888f6c381c66d97c23744fc476
Евгений Гудзенко < >
2017-02-07 21:19:24 +0200

    added two pages etc

--- js/main.js
+++ js/main.js
@@ -1,6 +1,23 @@
 $(function(){
 	$('#fullpage').fullpage({
-        anchors: ['header','teploceramic','katyatal'],
+        anchors: ['header','portfolio','contact'],
         scrollingSpeed: 1000,  
     });
+
+
+    var portfolioSlider = new Swiper ('.portfolio-slider', {
+    	pagination: '.portfolio-slider__pagination',
+    	slidesPerColumn: 3,
+    	slidesPerView: 4,
+    	spaceBetween: 10
+	});
+
+	(function formMagic(){
+		$('.contact__text-input, .contact__textarea').on('focus', function(){
+			$('.contact__title').removeClass('pulse-animated');
+			$('.contact__title').addClass('inset-shadow');
+			$(this).unbind('focus');
+		});
+
+	})();
 });
